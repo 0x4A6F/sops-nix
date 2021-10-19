cffd2d76fe354ba8726cc553fa9af06ae88d08c8
Janne Heß <janne@hess.ooo>
2021-10-19 18:04:34 +0200

    Fix secrets mount point and remove default

--- pkgs/sops-install-secrets/main.go
+++ pkgs/sops-install-secrets/main.go
@@ -44,7 +44,7 @@ type secret struct {
 
 type manifest struct {
 	Secrets           []secret `json:"secrets"`
-	SecretsMountPoint string   `json:"secretsMountpoint"`
+	SecretsMountPoint string   `json:"secretsMountPoint"`
 	SymlinkPath       string   `json:"symlinkPath"`
 	SSHKeyPaths       []string `json:"sshKeyPaths"`
 	GnupgHome         string   `json:"gnupgHome"`
@@ -491,12 +491,6 @@ func (app *appContext) validateSecret(secret *secret) error {
 
 func (app *appContext) validateManifest() error {
 	m := &app.manifest
-	if m.SecretsMountPoint == "" {
-		m.SecretsMountPoint = "/run/secrets.d"
-	}
-	if m.SymlinkPath == "" {
-		m.SymlinkPath = "/run/secrets"
-	}
 	if m.GnupgHome != "" {
 		errorFmt := "gnupgHome and %s were specified in the manifest. " +
 			"Both options are mutually exclusive."
